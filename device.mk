695972522db2309b3129a3521fda0da67cdf0895
orges <me@orgesified.ml>
2021-10-26 21:25:34 +0800

    sfo: config: Add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I1716319bb17b9236d07d44a2a7fa3c7640c54ddf

--- device.mk
+++ device.mk
@@ -310,6 +310,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 ifneq ($(QCPATH),)
 $(call inherit-product-if-exists, $(QCPATH)/prebuilt_HY11/target/product/msm8974/prebuilt.mk)
 endif
