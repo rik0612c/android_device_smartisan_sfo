384355cb793545a7c80bc7943434045392bde6b5
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    sfo: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device.mk
+++ device.mk
@@ -189,7 +189,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audiod \
-    audio.a2dp.default \
     audio_policy.msm8974 \
     audio.primary.msm8974 \
     audio.r_submix.default \
