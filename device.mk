ff925d2bdeba080eaf693a47689e71a9d44e6c2f
rik0612c <qq872638399@gmail.com>
2023-05-21 22:44:01 +0800

    sfo: Build android.hardware.configstore@1.1

--- device.mk
+++ device.mk
@@ -228,7 +228,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
+    android.hardware.memtrack@1.0-service \
+    android.hardware.configstore@1.1-service
 
 # RenderScript HAL
  PRODUCT_PACKAGES += \
