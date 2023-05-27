697a06dd8d438ae5fd38a630ee68de01cffac86c
rik0612c <qq872638399@gmail.com>
2023-05-27 23:42:06 +0800

    sfo: Change screen dpi to 480

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ TARGET_USES_ION                   := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS   := 3
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U | 0x02000000U | 0x02002000U
 TARGET_DISABLE_POSTRENDER_CLEANUP := true
+TARGET_SCREEN_DENSITY := 480
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
